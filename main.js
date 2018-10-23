3ae19584abace50c94a1e071da0e0ae6516e2479
EyasuG <jgebrehiwot@gmail.com>
2018-10-23 16:46:25 -0400

    This is the new website with all the html, css and js

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let myfavoriteSport = prompt('Do you think Soccer is my favorite Sport?')
+if (myfavoriteSport.toLowerCase() === 'yes' ||
+myfavoriteSport.toUpperCase() === 'YES') {
+  alert('You got it. It it my favorite Sport') 
+} else {
+  alert('Nice Try, Please try again')
+}
+let myfavoriteDes = prompt ('Are you sure Hawai is my favorite Destination')
+if (myfavoriteDes.toLowerCase() === 'yes' ||
+myfavoriteDes.toUpperCase() === 'YES') {
+  alert('Wow that is a dream place to be')
+ } else{
+    alert('Hey It is. How could you doubt that?') 
+  }
+let numberofsiblings = confirm('I have 6 siblings')
+if (numberofsiblings)
+console.log(numberofsiblings)
+let myfavoriteAuthor = prompt ('Do you know that Philip Yancey is my favorite author?')
+if (myfavoriteAuthor.toLowerCase() === 'yes' ||
+myfavoriteAuthor.toUpperCase() ==='YES') {
+alert(' You are expert at guessing')
+} else {
+  alert ('Somebody else is in your mind')
+}
+let mypassion = prompt ('I have strong passion for visiting new places')
+if (mypassion.toLowerCase() === 'yes' ||
+mypassion.toLocaleUpperCase() === 'YES') {
+  alert ('Of course ')}
+  else { alert('I do not think you know me well')
+}
+
+//let vacationsite =prompt( 'Hawai is my favorie Destination')
+//let vacationsite = confirm('Hawai is my favorite destination')
+//if(favoriteSport.toLowerCase() === 'yes' ||
+//favoriteSport.toUpperCase() === 'YES') {
+
+
+   //console.log('My favorite hero is ' + favoriteHero + 'and hate Marvel and I am a fantastic person') //Instead of interacting with the developer tool, we can interact with the user using alert 
